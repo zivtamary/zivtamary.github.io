c2066c0c7b0549758fb933c126850a841ab761a2
Ziv <zivtamary@gmail.com>
2019-05-07 19:34:47 +0300

    fix mobile menu bug

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,16 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
     document.getElementById("navbar").style.top = "-50px";
+   } else {
+    document.getElementById("navbar").style.top = "0px";
+   }
+   lastScrollTop = st;
+});
 
+window.onscroll = () => {
+  if (pageYOffset == 0) {
+    document.getElementById("navbar").style.top = "0px";
   }
-  prevScrollpos = currentScrollPos;
 }
