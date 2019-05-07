c2ad27aed02145172e39415711583180bebcc864
Ziv <zivtamary@gmail.com>
2019-05-07 19:18:55 +0300

    fix mobile menu bug

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0px";
-  } else if (prevScrollpos <= currentScrollPos) {
+  } else if (prevScrollpos <= currentScrollPos || currentScrollPos == 0) {
     document.getElementById("navbar").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
